12f67c699f68158126ec7441e3d29f6fe3e54eee
blacksanta <kei.01261026@gmail.com>
2015-08-15 15:11:54 +0900

    raw パス修正
    This reverts commit 0d9d6fe947ab0af4bfb9b5117e36853a32f98869.
    
    Revert "first commit"
    
    This reverts commit
    7c9d4f8d0aee740641a209d0bf7761286ba28477.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-(function(){var d=document;var s=d.createElement('script');s.src='https://rawgit.com/blacksanta/TwitterAutoFollow-Chrome-extension/master/keyuser_follow.js'; d.body.appendChild(s)})();
+(function(){var d=document;var s=d.createElement('script');s.src='https://rawgit.com/blacksanta/TwitterAutoFollow-Chrome-extension/master/src/keyuser_follow.js'; d.body.appendChild(s)})();
 
